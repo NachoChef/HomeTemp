14a910de6d1bd2303f7008617c7d8ba9aef133c1
Justin Jones <hock3ydud3@gmail.com>
2018-06-17 21:27:16 -0500

    record change for auto timestamp, added endpoint for retrieval

--- TemperatureAPI/src/main/java/com/home/temperatureapi/controller/TemperatureController.java
+++ TemperatureAPI/src/main/java/com/home/temperatureapi/controller/TemperatureController.java
@@ -1,5 +1,8 @@
 package com.home.temperatureapi.controller;
 
+import com.home.temperatureapi.model.TempRecord;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +14,17 @@ public class TemperatureController {
     //todo add jpa repository
 
     @RequestMapping(value = "/updateTemp/{room}/{temp}", method = RequestMethod.PUT)
-    public void updateTemp(@PathVariable("room") String room, @PathVariable("temp") String temp){
-        ;
+    public void updateTemp(@PathVariable("room") String room, @PathVariable("temp") String temp) {
+        TempRecord record = new TempRecord();
+        record.setTemperature(temp);
+        //repository.save(record);
+    }
+
+    @RequestMapping(value = "/getTemp/{room}")
+    public ResponseEntity<String> getRoomTemp(@PathVariable String room) {
+        String temp;
+        //temp = repository.findLastByRoom(room);
+        return new ResponseEntity<>(temp, HttpStatus.OK);
     }
 
 }
--- TemperatureAPI/src/main/java/com/home/temperatureapi/model/TempRecord.java
+++ TemperatureAPI/src/main/java/com/home/temperatureapi/model/TempRecord.java
@@ -1,9 +1,12 @@
 package com.home.temperatureapi.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
+@Data
 @Table(name = "temps")
 public class TempRecord {
     @Id
@@ -11,9 +14,16 @@ public class TempRecord {
     @Column(name = "id", updatable = false, nullable = false)
     private int id;
 
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "temp")
     private String temperature;
 
     @Column(name = "create_time")
     private Timestamp createTimestamp;
+
+    @PrePersist
+    protected void onCreate() {
+        createTimestamp = new Timestamp(System.currentTimeMillis());
+        ;
+    }
 }
